8cc81bd693b1cea79646a2382a55db2bf9f82a5d
tjausm <38621622+tjausm@users.noreply.github.com>
2022-04-30 17:23:15 -0600

    Working on tupple parser (as excercise)

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 #[macro_use]
 extern crate lalrpop_util;
 
+mod tupple;
+
 lalrpop_mod!(pub calculator); // synthesized by LALRPOP
 
 #[test]
 fn calculator1() {
-    assert!(calculator::ExprParser::new().parse("22").is_ok());
-    assert!(calculator::ExprParser::new().parse("((((22))))").is_ok());
-    assert!(calculator::ExprParser::new().parse("(22)").is_ok());
-    assert!(calculator::ExprParser::new().parse("2+(1+1)").is_ok());
-    //Wassert!(calculator::ExprParser::new().parse("2+((4+3))").is_ok());
-    assert!(calculator::ExprParser::new().parse("((22)").is_err());
-    //assert!(calculator::ExprParser::new().parse("((  22)").is_err());
+    assert!(calculator::ExprParser::new().parse("(2,2)").is_ok());
 }
 
 fn main() {
---
+++ src/tupple.rs
@@ -0,0 +1,9 @@
+
+pub struct Tupple(pub Data, pub Data);
+
+pub enum Data {
+    Number(i32),
+    //Tupple(Tupple)
+}
+
+
