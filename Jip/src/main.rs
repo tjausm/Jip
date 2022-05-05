ab5ef87f4bc466af202ddc1f795c6539eb230043
tjausm <38621622+tjausm@users.noreply.github.com>
2022-05-05 16:59:42 -0600

    Split the tests up and added console line support for checking concrete syntax using our parser (and added launch.json)

--- Jip/src/main.rs
+++ Jip/src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate lalrpop_util;
 
+use std::io;
+
 mod ast;
 
 lalrpop_mod!(pub parser); // synthesized by LALRPOP
@@ -13,26 +15,41 @@ mod tests {
     #[test]
     fn assignment() {
         assert!(parser::StatementsParser::new().parse("x := 2;").is_ok());
-        assert!(parser::StatementsParser::new().parse("2 := x;").is_err());
+        
     }
     #[test]
     fn declaration() {     
-        assert!(parser::StatementsParser::new().parse("if(true)x := 1; else x := 2;").is_ok());
+        assert!(parser::StatementsParser::new().parse("int x;").is_ok());
+        
 
     }
     #[test]
     fn statements() {
-        assert!(parser::StatementsParser::new().parse("int x;").is_ok());
-        assert!(parser::StatementsParser::new().parse("bool;").is_err());
-        assert!(parser::StatementsParser::new().parse("2").is_err());
+        assert!(parser::StatementsParser::new().parse("int x; x := 2; if(true)x := 1; else x := 2;").is_ok());
     }
-
-    
+    #[test]
+    fn faulty_input(){
+        assert!(parser::StatementsParser::new().parse("bool;").is_err());
+        assert!(parser::StatementsParser::new().parse("2 := x;").is_err());
+        assert!(parser::StatementsParser::new().parse("if (x := 1) x := 1; else x := 2;").is_err());
+    }
 
 }
 
 fn main() {
-    println!("Hello, world!");
+
+    println!("Welcome to Jip v0.1");
+    loop{
+        let mut input = String::new();
+
+        println!("\nPlease input a string:");
+        io::stdin().read_line(&mut input);
+    
+        match parser::StatementsParser::new().parse(&input){
+            Ok(_) => println!("Concrete syntax is correct"),
+            Err(e) => println!("Error: {})", e)
+        }
+    }
 }
 
 
