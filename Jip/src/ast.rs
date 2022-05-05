a1ff7caf873e40f1dc4c75fc3f0c843ee5a68366
tjausm <38621622+tjausm@users.noreply.github.com>
2022-05-05 15:19:38 -0600

    Implemented parser for the an subset of OOX

--- Jip/src/ast.rs
+++ Jip/src/ast.rs
@@ -1,7 +1,49 @@
-//TODO: kan concrete syntax 
+//naming convention: 
+// - syntactical labels are taken as is from Stefan's thesis
+// - each syntactical label's first symbol is transformed to uppercase (program -> Program)
+// - labels with only 1 ´option´ are type aliases, , 1 < options are enums
+
+pub enum Statements {
+    Cons(Statement, Box<Statements>),
+    Nil
+}
+
+pub enum Statement {
+    Declaration(Declaration),
+    Assignment(Assignment),
+    Ite(Box<Ite>)
+}
+
+pub type Declaration = (Nonvoidtype, Identifier);
+
+pub enum Nonvoidtype {
+    Primitivetype
+}
+
+pub enum Primitivetype {
+    Int,
+    Bool
+}
+
+
+pub type Assignment = (Lhs, Rhs);
+
+pub enum Lhs {
+    Identifier(String),    
+}
+
+pub enum Rhs {
+    Expr(Expr9)
+}
+
+
+
+
+pub type Ite = (Expr9, Statement, Statement);
+
 
 pub enum Expr9 {
-    Identifier(String),
+    Identifier(Identifier),
     Literal(Literal)
 }
 
@@ -9,3 +51,5 @@ pub enum Literal{
     Boolean(bool),
     Integer(i32)
 }
+
+type Identifier = String;
--- Jip/src/main.rs
+++ Jip/src/main.rs
@@ -5,15 +5,35 @@ mod ast;
 
 lalrpop_mod!(pub parser); // synthesized by LALRPOP
 
-#[test]
-fn calculator1() {
-    //test all data types, white spaces and nested tuples
-    assert!(parser::Expr9Parser::new().parse("42").is_ok());
-    assert!(parser::Expr9Parser::new().parse("ident").is_ok());
-    assert!(parser::Expr9Parser::new().parse("true").is_ok());
-    assert!(parser::Expr9Parser::new().parse("9 true").is_err());
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn assignment() {
+        assert!(parser::StatementsParser::new().parse("x := 2;").is_ok());
+        assert!(parser::StatementsParser::new().parse("2 := x;").is_err());
+    }
+    #[test]
+    fn declaration() {     
+        assert!(parser::StatementsParser::new().parse("if(true)x := 1; else x := 2;").is_ok());
+
+    }
+    #[test]
+    fn statements() {
+        assert!(parser::StatementsParser::new().parse("int x;").is_ok());
+        assert!(parser::StatementsParser::new().parse("bool;").is_err());
+        assert!(parser::StatementsParser::new().parse("2").is_err());
+    }
+
+    
+
 }
 
 fn main() {
     println!("Hello, world!");
 }
+
+
+
