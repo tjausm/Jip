437ed2277c28ed0aaebbba78c617161d46f46f77
tjausm <38621622+tjausm@users.noreply.github.com>
2022-06-29 14:02:24 +0200

    implemented class fields in parser

--- src/ast.rs
+++ src/ast.rs
@@ -15,14 +15,16 @@ pub type Class = (Identifier, Members);
 pub type Members = Vec<Member>;
 
 pub enum Member {
-    //Constructor(Constructor),
+    Constructor(Constructor),
     Method(Method),
-    //Field(Field)
+    Field(Field),
 }
+
+pub type Constructor = (Identifier, Parameters, Statements);
 
 pub enum Method {
     Static(Methodcontent),
-    Nonstatic(Methodcontent)
+    Nonstatic(Methodcontent),
 }
 
 //TODO: add args hier
@@ -31,6 +33,8 @@ pub type Methodcontent = (Type, Identifier, Parameters, Statements);
 pub type Parameters = Vec<Parameter>;
 
 pub type Parameter = (Type, Identifier);
+
+pub type Field = (Type, Identifier);
 
 pub type Statements = Vec<Statement>;
 
@@ -45,7 +49,7 @@ pub enum Statement {
     Block(Box<Statements>),
     Assert(Expression),
     Assume(Expression),
-    While(While)
+    While(While),
 }
 
 // Todo: add to syntax & semantics in thesis
@@ -59,20 +63,24 @@ pub type While = (Expression, Box<Statement>);
 pub enum Type {
     Void,
     Int,
-    Bool
+    Bool,
+    Classtype(Identifier),
 }
 
 pub type Assignment = (Lhs, Rhs);
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub enum Lhs {
     Identifier(String),
+    Accessfield(Identifier, Identifier),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub enum Rhs {
     Expression(Expression),
-    Invocation(Invocation)
+    Accessfield(Identifier, Identifier),
+    Invocation(Invocation),
+    Newobject(Identifier, Arguments),
 }
 
 //TODO: add args hier
@@ -84,11 +92,10 @@ pub type Ite = (Expression, Box<Statement>, Box<Statement>);
 
 #[derive(Clone)]
 pub enum Expression {
-
     //expression1
     Forall(Identifier, Box<Expression>),
     Exists(Identifier, Box<Expression>),
-    
+
     //expression2
     Implies(Box<Expression>, Box<Expression>),
 
@@ -99,7 +106,6 @@ pub enum Expression {
     //expression4
     EQ(Box<Expression>, Box<Expression>),
     NE(Box<Expression>, Box<Expression>),
-    
 
     //Expression5
     LT(Box<Expression>, Box<Expression>),
@@ -110,7 +116,7 @@ pub enum Expression {
     //Expression6
     Plus(Box<Expression>, Box<Expression>),
     Minus(Box<Expression>, Box<Expression>),
-    
+
     //Expression7
     Multiply(Box<Expression>, Box<Expression>),
     Divide(Box<Expression>, Box<Expression>),
@@ -135,26 +141,29 @@ pub type Identifier = String;
 
 impl fmt::Debug for Statement {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        
         match self {
-            Statement::DeclareAssign((t, id, rhs)) => write!(f, "{:?} {:?}", t, Statement::Assignment((Lhs::Identifier(id.to_string()), rhs.clone()))),
+            Statement::DeclareAssign((t, id, rhs)) => write!(
+                f,
+                "{:?} {:?}",
+                t,
+                Statement::Assignment((Lhs::Identifier(id.to_string()), rhs.clone()))
+            ),
             Statement::Declaration((t, id)) => write!(f, "{:?} {};", t, id),
-            Statement::Assignment((Lhs::Identifier(id), Rhs::Expression(expr))) => write!(f, "{} := {:?};", id, expr),
-            Statement::Assignment((Lhs::Identifier(id), Rhs::Invocation((class, fun, args)))) => write!(f, "{} := {}.{}({:?});", id, class, fun, args),
+            Statement::Assignment((lhs, rhs)) => write!(f, "{:?} := {:?};", lhs, rhs),
             Statement::Call((l, r, args)) => write!(f, "{}.{}({:?});", l, r, args),
-            Statement::Ite((cond, if_expr, else_expr)) => write!(f, "if ({:?}) then {:?} else {:?}", cond, if_expr, else_expr),
+            Statement::Ite((cond, if_expr, else_expr)) => {
+                write!(f, "if ({:?}) then {:?} else {:?}", cond, if_expr, else_expr)
+            }
             Statement::Return(expr) => write!(f, "return {:?};", expr),
             Statement::Block(stmts) => write!(f, "{{ {:?} }}", stmts),
             Statement::Assert(expr) => write!(f, "assert {:?};", expr),
             Statement::Assume(expr) => write!(f, "assume {:?};", expr),
             Statement::While((cond, body)) => write!(f, "while ({:?}) {:?}", cond, body),
         }
-
     }
 }
 impl fmt::Debug for Expression {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        
         match self {
             Expression::Forall(id, body) => write!(f, "forall {} : {:?}", id, body),
             Expression::Exists(id, body) => write!(f, "exists {} : {:?}", id, body),
@@ -177,18 +186,34 @@ impl fmt::Debug for Expression {
             Expression::Identifier(id) => write!(f, "{}", id),
             Expression::Literal(Literal::Boolean(val)) => write!(f, "{:?}", val),
             Expression::Literal(Literal::Integer(val)) => write!(f, "{:?}", val),
-        }   
-
+        }
+    }
+}
+impl fmt::Debug for Lhs {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Lhs::Accessfield(class, field) => write!(f, "{}.{}", class, field),
+            Lhs::Identifier(id) => write!(f, "{}", id),
+        }
+    }
+}
+impl fmt::Debug for Rhs {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Rhs::Expression(expr) => write!(f, "{:?};", expr),
+            Rhs::Accessfield(class, field) => write!(f, "{}.{};", class, field),
+            Rhs::Invocation((class, fun, args)) => write!(f, " {}.{}({:?});", class, fun, args),
+            Rhs::Newobject(class, args) => write!(f, "{}({:?});", class, args),
+        }
     }
 }
 impl fmt::Debug for Type {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        
         match self {
             Type::Void => write!(f, "void"),
             Type::Bool => write!(f, "bool"),
             Type::Int => write!(f, "int"),
-        }   
-
+            Type::Classtype(name) => write!(f, "{}", name),
+        }
     }
 }
